00380ba672743fe05621aa3169b58dff35e72830
Ravindra Shastri <ravindrashastri90@gmail.com>
2021-05-21 23:33:13 +0530

    add alert,prompt and confirm

--- block-BJaaao/code/index.js
+++ block-BJaaao/code/index.js
@@ -16,7 +16,11 @@ let num2 = 32;
 - Alert `The value of num1 is [num1] and the value of num2 is [num2]`
 - Alert `The sum of [num1] and [num2] is [num1 + num1]`
 */
-
+link language = JavaScript
+alert ("I am learning + language");
+alert (`The value of num1 is ${num1} and the value of num2 is ${num2}`);
+alert (num1, num2);
+alert (num1 + num2);
 /*
 2. Do the following using `alert`, `prompt` and `confirm` and `console`
 
@@ -32,6 +36,8 @@ let num2 = 32;
     Adult: [isAdult]
     Profession: [userProfession]` (it should be in three different lines)
 */
+prompt ("let user = userName")
+let  the proffession of the user = userProfession; 
 
 /*
 . Addition using prompt
