d517172c592ffac5e81c6516f198683bd4fb09b7
Ravindra Shastri <ravindrashastri90@gmail.com>
2021-05-27 23:54:50 +0530

    add using loops

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -5,9 +5,10 @@
 // Using `console.log` log all the values from 1 to 10.
 
 // Using `console.log` log all the evan values from 1 to 10.
-
+fckjkljk
+lpjuf
 // Using `console.log` log all the od values from 1 to 10.
-
+jhk;
 // Calculate the sum of all numbers from 1 to 10.
 
 // Log all the values from 1 to 10 using while loop
