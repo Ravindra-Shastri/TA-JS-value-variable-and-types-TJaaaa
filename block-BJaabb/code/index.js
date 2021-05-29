4742b47306aa9374928a90f9a118738ca8d619d5
Ravindra Shastri <ravindrashastri90@gmail.com>
2021-05-29 12:18:51 +0530

    making desicion in code

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,25 +5,55 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
-
+let user = prompt("Enter your name?");
+alert(user);
+let userName = user;
+console.log (user,userName);
+let age = 22, isLearning = true;
+user = "john";
+console.log (user, "user", userName ,"userName")
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA = +prompt("Enter first number");
+let numB = +prompt("Enter second number");
+let sum = numA + numB;
+alert(sum);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j += 2) {
+    console.log(j);
+}
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
-
+for (let i = 9;i >= 1; i--){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
 file:///home/khelomore/Documents/Altcampus/JavaScript/TA-JS-value-variable-and-types-TJaaaa/block-BJaaay
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
-dwjdfl;vmjkvk;lcmjkkf
+let finalString = "";
+let initalValue = 5;
+while(initalValue>=0){
+    finalString += initalValue;
+    initalValue --; 
+}
+console.log(finalString);
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
-
+let finalEvenString = "";
+let initalEvenValue = 10;
+while(initalEvenValue>=0){
+    if(initalEvenValue % 2 === 0){
+    finalEvenString += initalEvenValue;
+    }
+    initalEvenValue --; 
+}
+console.log(finalEvenString);
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
@@ -38,3 +68,10 @@ Example:
 "hello", -21; // false
 
 */
+let num1 = +prompt("Enter num one");
+let num2 = +prompt("Enter num two");
+if(isNaN(num1) || isNaN(num2)) {
+    alert(`Enter a valid value!`)
+} else{
+    alert(num1 === num2);
+}
